cabecfbb9b7b478879c977a4aeec2d52065b4a4a
jefferfonseca <63508641+jefferfonseca@users.noreply.github.com>
2021-05-13 11:10:18 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,122 +1,14 @@
+let T1 = 0
+let PT1 = 0
 let T2 = 0
 let PT2 = 0
 let T3 = 0
 let PT3 = 0
-let T1 = 0
-let PT1 = 0
-basic.forever(function () {
-    T2 = pins.analogReadPin(AnalogPin.P1)
-    PT2 = pins.map(
-    T2,
-    0,
-    1023,
-    0,
-    5
-    )
-    if (PT2 == 0) {
-        led.unplot(2, 0)
-        led.unplot(2, 1)
-        led.unplot(2, 2)
-        led.unplot(2, 3)
-        led.unplot(2, 4)
-    }
-    if (PT2 > 0 && PT2 <= 1) {
-        led.unplot(2, 0)
-        led.unplot(2, 1)
-        led.unplot(2, 2)
-        led.unplot(2, 3)
-        led.plot(2, 4)
-    } else {
-        if (PT2 > 1 && PT2 <= 2) {
-            led.unplot(2, 0)
-            led.unplot(2, 1)
-            led.unplot(2, 2)
-            led.plot(2, 3)
-            led.plot(2, 4)
-        } else {
-            if (PT2 > 2 && PT2 <= 3) {
-                led.unplot(2, 0)
-                led.unplot(2, 1)
-                led.plot(2, 2)
-                led.plot(2, 3)
-                led.plot(2, 4)
-            } else {
-                if (PT2 > 3 && PT2 <= 4) {
-                    led.unplot(2, 0)
-                    led.plot(2, 1)
-                    led.plot(2, 2)
-                    led.plot(2, 3)
-                    led.plot(2, 4)
-                } else {
-                    if (PT2 > 4 && PT2 <= 5) {
-                        led.plot(2, 0)
-                        led.plot(2, 1)
-                        led.plot(2, 2)
-                        led.plot(2, 3)
-                        led.plot(2, 4)
-                    }
-                }
-            }
-        }
-    }
-})
-basic.forever(function () {
-    T3 = pins.analogReadPin(AnalogPin.P2)
-    PT3 = pins.map(
-    T3,
-    0,
-    1023,
-    0,
-    5
-    )
-    if (PT3 == 0) {
-        led.unplot(4, 0)
-        led.unplot(4, 1)
-        led.unplot(4, 2)
-        led.unplot(4, 3)
-        led.unplot(4, 4)
-    }
-    if (PT3 > 0 && PT3 <= 1) {
-        led.unplot(4, 0)
-        led.unplot(4, 1)
-        led.unplot(4, 2)
-        led.unplot(4, 3)
-        led.plot(4, 4)
-    } else {
-        if (PT3 > 1 && PT3 <= 2) {
-            led.unplot(4, 0)
-            led.unplot(4, 1)
-            led.unplot(4, 2)
-            led.plot(4, 3)
-            led.plot(4, 4)
-        } else {
-            if (PT3 > 2 && PT3 <= 3) {
-                led.unplot(4, 0)
-                led.unplot(4, 1)
-                led.plot(4, 2)
-                led.plot(4, 3)
-                led.plot(4, 4)
-            } else {
-                if (PT3 > 3 && PT3 <= 4) {
-                    led.unplot(4, 0)
-                    led.plot(4, 1)
-                    led.plot(4, 2)
-                    led.plot(4, 3)
-                    led.plot(4, 4)
-                } else {
-                    if (PT3 > 4 && PT3 <= 5) {
-                        led.plot(4, 0)
-                        led.plot(4, 1)
-                        led.plot(4, 2)
-                        led.plot(4, 3)
-                        led.plot(4, 4)
-                    }
-                }
-            }
-        }
-    }
-})
-basic.forever(function () {
+let PromT1 = 0
+let PromT2 = 0
+let PromT3 = 0
+let PromTotal = 0
+input.onButtonPressed(Button.A, function () {
     T1 = pins.analogReadPin(AnalogPin.P0)
     PT1 = pins.map(
     T1,
@@ -172,4 +64,127 @@ basic.forever(function () {
             }
         }
     }
+    T2 = pins.analogReadPin(AnalogPin.P1)
+    PT2 = pins.map(
+    T2,
+    0,
+    1023,
+    0,
+    5
+    )
+    if (PT2 == 0) {
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 3)
+        led.unplot(2, 4)
+    }
+    if (PT2 > 0 && PT2 <= 1) {
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 3)
+        led.plot(2, 4)
+    } else {
+        if (PT2 > 1 && PT2 <= 2) {
+            led.unplot(2, 0)
+            led.unplot(2, 1)
+            led.unplot(2, 2)
+            led.plot(2, 3)
+            led.plot(2, 4)
+        } else {
+            if (PT2 > 2 && PT2 <= 3) {
+                led.unplot(2, 0)
+                led.unplot(2, 1)
+                led.plot(2, 2)
+                led.plot(2, 3)
+                led.plot(2, 4)
+            } else {
+                if (PT2 > 3 && PT2 <= 4) {
+                    led.unplot(2, 0)
+                    led.plot(2, 1)
+                    led.plot(2, 2)
+                    led.plot(2, 3)
+                    led.plot(2, 4)
+                } else {
+                    if (PT2 > 4 && PT2 <= 5) {
+                        led.plot(2, 0)
+                        led.plot(2, 1)
+                        led.plot(2, 2)
+                        led.plot(2, 3)
+                        led.plot(2, 4)
+                    }
+                }
+            }
+        }
+    }
+    T3 = pins.analogReadPin(AnalogPin.P2)
+    PT3 = pins.map(
+    T3,
+    0,
+    1023,
+    0,
+    5
+    )
+    if (PT3 == 0) {
+        led.unplot(4, 0)
+        led.unplot(4, 1)
+        led.unplot(4, 2)
+        led.unplot(4, 3)
+        led.unplot(4, 4)
+    }
+    if (PT3 > 0 && PT3 <= 1) {
+        led.unplot(4, 0)
+        led.unplot(4, 1)
+        led.unplot(4, 2)
+        led.unplot(4, 3)
+        led.plot(4, 4)
+    } else {
+        if (PT3 > 1 && PT3 <= 2) {
+            led.unplot(4, 0)
+            led.unplot(4, 1)
+            led.unplot(4, 2)
+            led.plot(4, 3)
+            led.plot(4, 4)
+        } else {
+            if (PT3 > 2 && PT3 <= 3) {
+                led.unplot(4, 0)
+                led.unplot(4, 1)
+                led.plot(4, 2)
+                led.plot(4, 3)
+                led.plot(4, 4)
+            } else {
+                if (PT3 > 3 && PT3 <= 4) {
+                    led.unplot(4, 0)
+                    led.plot(4, 1)
+                    led.plot(4, 2)
+                    led.plot(4, 3)
+                    led.plot(4, 4)
+                } else {
+                    if (PT3 > 4 && PT3 <= 5) {
+                        led.plot(4, 0)
+                        led.plot(4, 1)
+                        led.plot(4, 2)
+                        led.plot(4, 3)
+                        led.plot(4, 4)
+                    }
+                }
+            }
+        }
+    }
+    PromT1 = PromT1 + T1
+    PromT2 = PromT2 + T2
+    PromT3 = PromT3 + T3
+})
+input.onButtonPressed(Button.AB, function () {
+    PromTotal = (PromT1 + PromT2 + PromT3) / 3
+    basic.showNumber(PromTotal)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("PromT1")
+    basic.showNumber(PromT1)
+    basic.showString("PromT2")
+    basic.showNumber(PromT2)
+    basic.showString("PromT2")
+    basic.showNumber(PromT3)
 })
